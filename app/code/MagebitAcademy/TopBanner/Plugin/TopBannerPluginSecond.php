063eb3418b69c06d6bc851229daf776ec4e71e00
MarisSuss <marissuss@gmail.com>
2025-10-16 16:23:13 +0300

    Add second plugin

---
+++ app/code/MagebitAcademy/TopBanner/Plugin/TopBannerPluginSecond.php
@@ -0,0 +1,85 @@
+<?php
+/**
+* @copyright Copyright (c) 2025 Magebit, Ltd. (https://magebit.com/)
+* @author    Magebit <info@magebit.com>
+* @license   MIT
+*/
+
+
+declare(strict_types=1);
+
+
+namespace MagebitAcademy\TopBanner\Plugin;
+
+
+use MagebitAcademy\TopBanner\Block\TopBanner;
+use Psr\Log\LoggerInterface;
+
+
+class TopBannerPluginSecond
+{
+   /**
+    * @param LoggerInterface $logger
+    */
+   public function __construct(
+       private readonly LoggerInterface $logger
+   ) {
+   }
+
+
+ /**
+   * Before plugin for getDummy method
+   *
+   * This method executes before the original getDummy() method.
+   *
+   * @param TopBanner $subject
+   * @param string $getDummy
+   * @return array
+   */
+   public function beforeGetDummy(TopBanner $subject, string $dummyText): array
+   {
+       $this->logger->info('PLUGIN: SO-100 beforeGetDummy execution');
+       $dummyText .= ', SO-100 before plugin executed';
+       return [$dummyText];
+   }
+
+
+   /**
+    * After plugin for getDummy method
+    *
+    * This method executes after the original getDummy() method.
+    *
+    * @param TopBanner $subject
+    * @param string $result
+    * @return string
+    */
+   public function afterGetDummy(TopBanner $subject, string $result): string
+   {
+       $this->logger->info('PLUGIN: SO-100 afterGetDummy execution');
+       return $result . ', SO-100 after plugin executed';
+   }
+
+
+   /**
+    * Around plugin for getDummy method
+    *
+    * This method wraps the original getDummy() method and can control its execution.
+    *
+    * @param TopBanner $subject
+    * @param callable $proceed
+    * @param string $dummyText
+    * @return string
+    */
+   public function aroundGetDummy(TopBanner $subject, callable $proceed, string $dummyText): string
+   {
+       $this->logger->info('PLUGIN: SO-100 aroundGetDummy execution - before proceed');
+       $dummyText .= ', SO-100 around plugin before proceed';
+
+
+       $result = $proceed($dummyText);
+
+
+       $this->logger->info('PLUGIN: SO-100 aroundGetDummy execution - after proceed');
+       return $result . ', SO-100 around plugin after proceed';
+   }
+}
